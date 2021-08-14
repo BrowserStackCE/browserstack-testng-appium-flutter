283174e2943b5f07b21da390738d9fde369d833f
rushabhshroff <rushabh.s@browserstack.com>
2021-08-14 11:33:40 +0530

    Removed Sample Case for Sample Flutter App

--- android/testng-examples/src/test/java/com/browserstack/appium_flutter/SampleTest.java
+++ android/testng-examples/src/test/java/com/browserstack/appium_flutter/SampleTest.java
@@ -8,13 +8,8 @@ import org.testng.annotations.Test;
 public class SampleTest extends BaseTestClass {
     @Test
     public void basicTest () {
-        String buttonFinderKey = "increment";
-        MobileElement counterTextFinder = find.byValueKey("counter");
-        MobileElement addButton = (MobileElement)driver.executeScript("flutter:waitFor", find.byValueKey(buttonFinderKey), 30);;
-        Assert.assertEquals(counterTextFinder.getText(), "0");
-        addButton.click();
-        Assert.assertEquals(counterTextFinder.getText(), "1");
-        addButton.click();
-        Assert.assertEquals(counterTextFinder.getText(), "2");
+        //Start Writing Test Case Here
+        //driver & find is available using inheritance
+        Assert.assertTrue(1==1);
     }
 }
