de3d627db14abd0cbc21702e050b6ded09b64140
rushabhshroff <rushabh.s@browserstack.com>
2021-08-16 10:33:24 +0530

    app was already being set in while loop for caps

--- android/testng-examples/src/test/java/com/browserstack/appium_flutter/BaseTestClass.java
+++ android/testng-examples/src/test/java/com/browserstack/appium_flutter/BaseTestClass.java
@@ -54,10 +54,8 @@ public class BaseTestClass {
         }
 
         String app = System.getenv("BROWSERSTACK_APP_ID");
-        if (app == null) {
-            app = (String) ((Map<?, ?>) config.get("capabilities")).get("app");
-        } else {
-            capabilities.setCapability("app", app);
+        if(app != null){
+            capabilities.setCapability("app",app);
         }
 
         driver = new AndroidDriver(new URL("http://"+username+":"+accessKey+"@"+config.get("server")+"/wd/hub"), capabilities);
